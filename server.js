2cdaad00262b95630230c1442fd7ec6491cf0a0f
padungaz <padung1999@gmail.com>
2024-02-17 15:58:52 +0700

    feat: lab 4.1

--- server.js
+++ server.js
@@ -1,44 +1,16 @@
-const http = require('http');
-let users = [];
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const express = require('express');
+const app = express();
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/add-user" method="POST"><input type="text" name="username"><button type="submit">Add User</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/users' && method === 'GET') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Users</title><head>');
-        res.write('<body><ul>');
-        users.forEach(user => {
-            res.write(`<li>${user}</li>`);
-        });
-        res.write('</ul></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/add-user' && method === 'POST') {
-        const chunks = [];
-        req.on('data', chunk => {
-            chunks.push(chunk);
-        });
-        req.on('end', () => {
-            const body = Buffer.concat(chunks).toString();
-            const username = body.split('=')[1];
-            users.push(username);
-            res.statusCode = 302;
-            res.setHeader('Location', '/users');
-            return res.end();
-        });
-    }
+app.get('/', (req, res) => {
+    res.send('<p>The Middleware that handles just /</p>');
 });
 
-server.listen(3000);
+
+app.get('/users', (req, res) => {
+    res.send('<p>The Middleware that handles just /users</p>');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
