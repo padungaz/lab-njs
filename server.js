c730b04bf050cf0925a1a7f6666928ff1882b300
padungaz <padung1999@gmail.com>
2024-02-21 19:05:33 +0700

    lab 5.1

--- server.js
+++ server.js
@@ -15,23 +15,57 @@
 //     console.log(`Server is running on port ${PORT}`);
 // });
 
-const express = require('express');
+
+// lab 4.2
+
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// });
+
+// app.get('/users', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'users.html'));
+// });
+
+// app.use((req, res, next) => {
+//     res.status(404).render('404');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+// lab 5.1
+
 const path = require('path');
 
-const app = express();
-const PORT = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.get('/users', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'users.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+app.listen(3000);
