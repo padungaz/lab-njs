bc13ef6d757c8d97d4f03f589b64ce4c66bc7a27
padungaz <padung1999@gmail.com>
2024-02-21 14:26:12 +0700

    feat: lab 4.2

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('<p>The Middleware that handles just /</p>');
+// });
+
+
+// app.get('/users', (req, res) => {
+//     res.send('<p>The Middleware that handles just /users</p>');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('<p>The Middleware that handles just /</p>');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-
 
 app.get('/users', (req, res) => {
-    res.send('<p>The Middleware that handles just /users</p>');
+    res.sendFile(path.join(__dirname, 'views', 'users.html'));
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
